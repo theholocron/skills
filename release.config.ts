c83e704438ee551c001964964eab94df00237d5f
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-21 21:38:00 -0700

    fix: add publishCmd to release config to enable npm publishing (#10)
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- release.config.ts
+++ release.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from "@theholocron/semantic-release-config";
 
 export default defineConfig({
 	branches: ["main", { name: "alpha", prerelease: true }],
+	exec: {
+		publishCmd:
+			"pnpm publish --access public --no-git-checks --tag ${nextRelease.channel || 'latest'}",
+	},
 });
